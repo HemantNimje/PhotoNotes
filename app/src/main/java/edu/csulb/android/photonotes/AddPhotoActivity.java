94c44591f68c27e665af2b2ea15c972570c4f2b3
Hemant <nimje.hemant@gmail.com>
2017-03-08 14:17:04 -0800

    Add layout for AddPhotoActivity. Attach addPhotoActivity on floating action button click.

---
+++ app/src/main/java/edu/csulb/android/photonotes/AddPhotoActivity.java
@@ -0,0 +1,16 @@
+package edu.csulb.android.photonotes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hemant on 3/8/2017.
+ */
+
+public class AddPhotoActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_photo);
+    }
+}
