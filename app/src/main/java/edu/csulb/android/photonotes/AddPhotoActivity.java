8ebaf4740369db5c5c1a8dd5dc5182a45702ec3d
Hemant <nimje.hemant@gmail.com>
2017-03-09 18:23:53 -0800

    Add functionality to launch the camera when add button is clicked from the add_photo page. Show the image returned by the camera on the image view.

--- app/src/main/java/edu/csulb/android/photonotes/AddPhotoActivity.java
+++ app/src/main/java/edu/csulb/android/photonotes/AddPhotoActivity.java
@@ -1,16 +1,72 @@
 package edu.csulb.android.photonotes;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.ActionMenuItemView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by Hemant on 3/8/2017.
  */
 
 public class AddPhotoActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_photo);
+
+        Button addButton = (Button) findViewById(R.id.button_add);
+        Button saveButton = (Button) findViewById(R.id.button_save);
+
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Add Button Clicked", Toast.LENGTH_LONG).show();
+
+                dispatchTakePictureIntent();
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Save Button Clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
+
+    /* Set the imageView with the image that is returned by the camera */
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView capturedImage = (ImageView) findViewById(R.id.captured_image);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            capturedImage.setImageBitmap(imageBitmap);
+        }
+    }
+
+    /* Launch the camera intent to capture the image */
+    public void dispatchTakePictureIntent() {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
 }
