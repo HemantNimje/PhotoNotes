abfa95227001d56eb3963faf3c02952a053cf138
Hemant <nimje.hemant@gmail.com>
2017-03-10 12:07:07 -0800

    Create file where captured image is to be loaded. Set the imageview using the file of the captured image.

--- app/src/main/java/edu/csulb/android/photonotes/AddPhotoActivity.java
+++ app/src/main/java/edu/csulb/android/photonotes/AddPhotoActivity.java
@@ -2,14 +2,26 @@ package edu.csulb.android.photonotes;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.menu.ActionMenuItemView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.R.attr.data;
 
 /**
  * Created by Hemant on 3/8/2017.
@@ -18,6 +30,7 @@ import android.widget.Toast;
 public class AddPhotoActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,6 +59,37 @@ public class AddPhotoActivity extends AppCompatActivity {
 
     }
 
+
+    /* Launch the camera intent to capture the image */
+    public void dispatchTakePictureIntent() {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Make sure that there is a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            // Create a file where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException exception) {
+                // Error occurred while creating the file
+                Toast.makeText(this, "Error occurred while creating the file", Toast.LENGTH_SHORT).show();
+            }
+
+            // Continue only if the File was created successfully
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            } else {
+                Toast.makeText(this, "fault", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     /* Set the imageView with the image that is returned by the camera */
 
     @Override
@@ -53,20 +97,25 @@ public class AddPhotoActivity extends AppCompatActivity {
         ImageView capturedImage = (ImageView) findViewById(R.id.captured_image);
 
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //Bundle extras = data.getExtras();
+            //Bitmap imageBitmap = (Bitmap) extras.get("data");
+            Bitmap imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
             capturedImage.setImageBitmap(imageBitmap);
         }
     }
 
-    /* Launch the camera intent to capture the image */
-    public void dispatchTakePictureIntent() {
+    private File createImageFile() throws IOException {
 
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        // Save a file: path for the use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
-
 
 }
